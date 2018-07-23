afd5844a5cef325bb364960751e3f716850187a0
mayongjie <mayong_jie@163.com>
2018-07-23 15:55:21 +0800

    spring config

---
+++ blog-config/src/main/java/com/cll/mayj/BlogConfigApplication.java
@@ -0,0 +1,13 @@
+package com.cll.mayj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//spring-boot开启注解
+@SpringBootApplication
+public class BlogConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogConfigApplication.class, args);
+    }
+}
--- blog-eureka/src/main/java/com/cll/mayj/BlogEurekaApplication.java
+++ blog-eureka/src/main/java/com/cll/mayj/BlogEurekaApplication.java
@@ -2,8 +2,12 @@ package com.cll.mayj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+//spring-boot启动注解
 @SpringBootApplication
+//spring-cloud服务注解
+@EnableEurekaServer
 public class BlogEurekaApplication {
 
     public static void main(String[] args) {
